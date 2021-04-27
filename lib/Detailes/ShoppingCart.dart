71a9bbc400dd67845f017b7b7848ad3bd15f2bac
Musthafa Ap <musthafamohammed398@gmail.com>
2021-04-27 08:11:25 +0530

    Favorites function is added

--- lib/Detailes/ShoppingCart.dart
+++ lib/Detailes/ShoppingCart.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:planta/Favorites/Favorites.dart';
 import 'package:planta/Homepage/HomeDetailesPage.dart';
 import 'package:planta/main.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -7,18 +7,18 @@ import 'package:rflutter_alert/rflutter_alert.dart';
 
 import 'Detailes.dart';
 
-class ShoppingCart extends StatelessWidget {
-
-final int Count;
+class ShoppingCart extends StatefulWidget {
+  final int Count;
   final String name;
   final BuildContext context;
-  ShoppingCart({this.name,this.context,this.Count});
+  ShoppingCart({this.name, this.context, this.Count});
 
+  @override
+  _ShoppingCartState createState() => _ShoppingCartState();
+}
 
-
-
-
-
+class _ShoppingCartState extends State<ShoppingCart> {
+  bool liked = false;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -27,87 +27,73 @@ final int Count;
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Container(
-            width: 50,
-            height: 50,
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(color: Colors.black)
-            ),
-            child: IconButton(icon: Icon(Icons.favorite,color: Colors.red),onPressed: (){print(name);},)
-          ),
-          FlatButton.icon(onPressed: (){return Alert(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.black)),
+              child: IconButton(
+                icon: Icon(liked ? Icons.favorite : Icons.favorite_border,
+                    color: liked ? Colors.red : Colors.black),
+                onPressed: () {
+                  setState(() {
+                    liked = !liked;
 
-            context: context,
-            title:'ORDER',
-            desc: '(Send an email)',
-            content: Text('Are you sure to buy this plant ?'),
-            buttons: [
-              DialogButton(
-                child: Text('NO'),
-
-                onPressed: (){
-                 Navigator.of(context).pop();
-                 },
-color: Colors.white,border:Border.all(color: Colors.green),
-              ),
-              DialogButton(
-
-                child: Text("YES"),
-                onPressed: ( ){
-
-                  Navigator.of(context).pop();
-
-
-_launchEmail();
-
-
-
-
+                  });
 
                 },
-                color: Colors.green,
+              )),
+          FlatButton.icon(
+              onPressed: () {
+                return Alert(
+                    context: context,
+                    title: 'ORDER',
+                    desc: '(Send an email)',
+                    content: Text('Are you sure to buy this plant ?'),
+                    buttons: [
+                      DialogButton(
+                        child: Text('NO'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        color: Colors.white,
+                        border: Border.all(color: Colors.green),
+                      ),
+                      DialogButton(
+                        child: Text("YES"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
 
-              )
-            ]
-
-          ).show();
-
-
-
-
-
-
-          }, icon: Icon(Icons.shopping_basket,size: 25,), label: Text("Buy Now",style: TextStyle(fontSize: 25),),shape: StadiumBorder(),
-              color: Colors.green,padding: EdgeInsets.only(left: 75,right: 75,top: 8,bottom: 8))
+                          _launchEmail();
+                        },
+                        color: Colors.green,
+                      )
+                    ]).show();
+              },
+              icon: Icon(
+                Icons.shopping_basket,
+                size: 25,
+              ),
+              label: Text(
+                "Buy Now",
+                style: TextStyle(fontSize: 25),
+              ),
+              shape: StadiumBorder(),
+              color: Colors.green,
+              padding: EdgeInsets.only(left: 75, right: 75, top: 8, bottom: 8))
         ],
       ),
     );
   }
-  void _launchEmail()async{
 
-    var url = 'mailto:musthafamohammed398@gmail.com?subject=For ordering &body=Plant name : $name,   Count : $Count'
+  void _launchEmail() async {
+    var url =
+        'mailto:musthafamohammed398@gmail.com?subject=For ordering &body=Plant name : $widget.name,   Count : $widget.Count'
         '';
-    if(await canLaunch(url)){
+    if (await canLaunch(url)) {
       await launch(url);
-    }
-    else{
+    } else {
       throw 'Could not send mail';
     }
   }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
